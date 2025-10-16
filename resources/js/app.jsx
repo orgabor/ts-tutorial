ed4436d18b23b7b473137c827741f7c73cd61115
Joe Tannenbaum <joe@joe.codes>
2025-10-16 10:35:14 -0400

    Update app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,18 +1,18 @@
 import { createInertiaApp } from '@inertiajs/react';
 import { configureEcho } from '@laravel/echo-react';
 import { createRoot } from 'react-dom/client';
-
-const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true });
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 configureEcho({
     broadcaster: 'reverb',
 });
 
 createInertiaApp({
-    resolve: (name) => {
-        const page = pages[`./Pages/${name}.jsx`];
-        return page && page.default ? page.default : page;
-    },
+    resolve: (name) =>
+        resolvePageComponent(
+            `./pages/${name}.jsx`,
+            import.meta.glob('./pages/**/*.jsx'),
+        ),
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
     },
